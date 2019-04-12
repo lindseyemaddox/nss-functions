a250fd5c3ce283df3420e770bddb3e7146511dd8
Lindsey Maddox <lindseyemaddox@gmail.com>
2019-04-11 21:53:49 -0500

    work on battle of bands

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,10 +1,7 @@
 
 console.log("hello there functions");
 
-// Write a program that prints the numbers from 1 to 100
-// For multiples of five print "Chicken" instead of the number
-// for the multiples of seven print "Monkey"
-// For numbers which are multiples of both five and seven print "ChickenMonkey"
+//////////////////// CHICKENMONKEY ////////////////////
 
 // is this necessary?
 let currentNumber; 
@@ -30,3 +27,23 @@ for (let currentNumber = 1; currentNumber <= 100; currentNumber++ ) {
 }
 
 
+//////////////////// BATTLE OF THE BANDS ////////////////////
+
+// Write a function that accepts any band name as an argument. 
+// The function will increment a global variable by one each time it is invoked, 
+// and return that number and the band name concatenated together.
+
+let bandNumber = 1
+
+const takeNumber = function () {
+    /*
+        Write your awesome code here. See comments
+        below for what should be returned.
+    */
+}
+
+const scum = takeNumber("Galactic Scum")
+console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs")
+console.log(under)  // This should print "2. Underdogs" in the console
