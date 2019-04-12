f3efd4274751ea02fbaf7244b8e888abfe6f1d9a
Lindsey Maddox <lindseyemaddox@gmail.com>
2019-04-11 20:54:02 -0500

    working chickens and monkeys and chickenmonkeys

--- scripts/functions.js
+++ scripts/functions.js
@@ -6,19 +6,27 @@ console.log("hello there functions");
 // for the multiples of seven print "Monkey"
 // For numbers which are multiples of both five and seven print "ChickenMonkey"
 
-for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
-  console.log(currentNumber);
-  // divide by 5, and check for remainder of 0
-  if (currentNumber % 5 === 0) {
-      console.log("chicken");
-  }
-  // divide by 7, and check for remainder of 0
-  if (currentNumber % 7 === 0) {
-    console.log("monkey");
-  }
-  // same thing for both 5 and 7
-  if (currentNumber % 5 === 0 && currentNumber % 7 === 0) {
+// is this necessary?
+let currentNumber; 
+
+for (let currentNumber = 1; currentNumber <= 100; currentNumber++ ) {
+
+  if (currentNumber % 5 === 0 && currentNumber % 7 === 0 ) {
     console.log("chickenmonkey");
   }
+    
+  else if (currentNumber % 5 === 0 ) {
+    console.log("chicken");
+  }
+
+  else if (currentNumber % 7 === 0) {
+    console.log("monkey");
+  }
+
+  else {
+    console.log(currentNumber);
+  } 
+    
 }
+
 
