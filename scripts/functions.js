742baa574af445f351935a238b461d3c393d4df1
Lindsey Maddox <lindseyemaddox@gmail.com>
2019-04-13 21:02:53 -0500

    battle of the bands sorted

--- scripts/functions.js
+++ scripts/functions.js
@@ -3,7 +3,6 @@ console.log("hello there functions");
 
 //////////////////// CHICKENMONKEY ////////////////////
 
-// is this necessary?
 let currentNumber; 
 
 for (let currentNumber = 1; currentNumber <= 100; currentNumber++ ) {
@@ -29,22 +28,16 @@ for (let currentNumber = 1; currentNumber <= 100; currentNumber++ ) {
 
 //////////////////// BATTLE OF THE BANDS ////////////////////
 
-// Write a function that accepts any band name as an argument. 
-// The function will increment a global variable by one each time it is invoked, 
-// and return that number and the band name concatenated together.
+let bandNumber = 0;
 
-let bandNumber = 1;
-
-const takeNumber = function assignNumber (...restArgs) {
-  // for (let bandNumber = 1; bandNumber >= 1; bandNumber++) => i
-  // forEach bandNumber (restArgs.length);22222222222
+const takeNumber = (bandName) => {
+  bandNumber += 1;
+  console.log(`${bandNumber}. ${bandName}`);
 }
 
-const scum = takeNumber("Galactic Scum")
-console.log(scum)  // This should print "1. Galactic Scum" in the console
-
-const under = takeNumber("Underdogs")
-console.log(under)  // This should print "2. Underdogs" in the console
+takeNumber("Band One");
+takeNumber("Band Two");
+takeNumber("Band Three");
 
 
 //////////////////// COOKOUT ////////////////////
@@ -75,7 +68,7 @@ const steak = {
 	cooked: false,
 }
 
-// An array that is grouping the objects together.
+// array that is grouping the objects together
 const foods = [hamburger, zucchini, chickenBreast, corn, steak];
 
 // An empty array that will store the objects after the `grill()` function cooks the food.
@@ -89,7 +82,8 @@ function grill (currentObject) {
   cookedFood.push(currentObject);
 };
 
-// iterate over the array of foods and invoke the function for each item 
+// iterate over the array of foods and 
+// invoke the function for each item 
 // so that the cookedFood array contains all of the items after they are cooked.
 
 // for (let i = 0; i <= foods.length; i++) {
