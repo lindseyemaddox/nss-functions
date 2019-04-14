abfc01375207a43708bd879db89919d6d9f58745
Lindsey Maddox <lindseyemaddox@gmail.com>
2019-04-14 05:16:57 -0500

    cookout, overly excited

--- scripts/functions.js
+++ scripts/functions.js
@@ -94,11 +94,11 @@ console.log(cookedFood);
 // create a single JavaScript function named addExcitement 
 // that will use console.log() to print out a sentence to the browser console.
 
-// function addExcitement() {
-//   console.log("This is a sentence.");
-// }
+function addExcitement() {
+  console.log("This is a sentence.");
+}
 
-// addExcitement();
+addExcitement();
 
 
 //////////////////// STACKING WORDS ////////////////////
